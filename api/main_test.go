3194aa725e0bc55ad785d3a304e2f1ef5f4e0417
tmkinoue <tmkinoue@outlook.com>
2021-10-05 20:08:00 +0900

    add test pattern

--- api/main_test.go
+++ api/main_test.go
@@ -5,11 +5,18 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPingRoute(t *testing.T) {
+func setupTestRouter() *gin.Engine {
+	gin.SetMode(gin.TestMode)
 	router := setupRouter()
+	return router
+}
+
+func TestPingRoute(t *testing.T) {
+	router := setupTestRouter()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/ping", nil)
@@ -17,4 +24,15 @@ func TestPingRoute(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "pong", w.Body.String())
+}
+
+func TestNotFoundRoute(t *testing.T) {
+	router := setupTestRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/404NotFound", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 404, w.Code)
+	assert.Equal(t, "404 page not found", w.Body.String())
 }
